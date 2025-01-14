23db023b0dc54e5024194c61c36db5f29e72940b
pvega7721 <pvega7721@ceuandalucia.es>
2025-01-14 20:40:18 +0100

    Actualiza contenido y estilos en archivos HTML y JavaScript; añade nueva estructura y funcionalidad a las actividades de jQuery.

--- Tema4/JQuery/Prueba/js/funciones.js
+++ Tema4/JQuery/Prueba/js/funciones.js
@@ -1,10 +1,41 @@
-jQuery(document).ready(function ($) {
+$(document).ready(function () {
   console.log("test");
   $(".bloque").hide();
-
-  $(".capa").hide();
-
   $("button").click(function () {
     $(this).hide();
   });
+  $("button").on("mouseenter", function () {
+    $(this).hide();
+  });
+  let contenido = $("#contenido");
+  //Método .css()
+  let fondoBackground = $(".contenido2").css("background-color");
+  let colorFondo = "#343434";
+  /*
+  <style>
+      .contenido{ background-color: green; }
+  </style>
+  */
+  $(".contenido").css("background-color", colorFondo);
+  //Quiero más de un atributo
+  $(".contenido,button").css({
+    "background-color": "yellow",
+    "font-size": "3em",
+    color: "red",
+  });
+  /* Actividad 2 */
+  //$('h2').hide();
+  /*
+ $('h2').on("mouseenter",function(){
+  $(this).hide();//solo se oculta el selector h2 que llama al evento
+  $('h2').hide();//oculta todos los h2
+ });
+ */
+  $("h2").on("click", function () {
+    $("h2").hide();
+  });
 });
+//No definido
+/*jQuery(document).ready(function($){
+  console.log("test");
+});*/
