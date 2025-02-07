4b77b3c6a0448d6940345d564c5ae8f68ac0a1c1
pvega7721 <pvega7721@ceuandalucia.es>
2025-02-07 18:58:18 +0100

    .

--- Tema4/JQuery/Actividades_JQuery/Actividad1/js/accion.js
+++ Tema4/JQuery/Actividades_JQuery/Actividad1/js/accion.js
@@ -49,295 +49,73 @@ $(document).ready(function () {
     });
   });
 
-  var inputNombre5 = $("#inputNombre5");
-  var inputMail5 = $("#inputCorreo5");
-  var inputContr5 = $("#inputContrasena5");
-  var inputConfContr5 = $("#inputConfContrasena5");
+  var inputNombre5 = $("#nombre5");
+  var inputMail5 = $("#correo5");
+  var inputContr5 = $("#contrasena5");
+  var inputConfContr5 = $("#confContrasena5");
   var botonCrear5 = $("#crear5");
-  var formulario5 = $("#formulario5");
-
-  //Al pulsar el botón Crear, si todo ha ido bien mostrará un mensaje de éxito                                    -----                                                                           ---
-  botonCrear5.on("click", function (e) {
-    e.preventDefault();
-    if (inputContr5.val() !== inputConfContr5.val()) {
-      $("#coincidirContrasena5").prop("hidden", false);
-    } else {
-      formulario5.slideUp(500, function () {
-        contenedor6.slideDown(500);
-      });
-    }
-  });
-
-  var ojoAbierto5 = $("#imagenOjoAbierto5");
-  var ojoCerrado5 = $("#imagenOjoCerrado5");
-
-  //Al pulsar el ojo, se muestra la contraseña y se cambia la imagen
-  ojoCerrado5.on("click", function () {
-    if (inputContr5.attr("type") === "password") {
-      inputContr5.attr("type", "text");
-      ojoAbierto5.prop("hidden", false);
-      ojoCerrado5.prop("hidden", true);
-    }
-  });
-
-  ojoAbierto5.on("click", function () {
-    if (inputContr5.attr("type") === "text") {
-      inputContr5.attr("type", "password");
-      ojoAbierto5.prop("hidden", true);
-      ojoCerrado5.prop("hidden", false);
-    }
-  });
-
-  var ojoAbiertoConf5 = $("#imagenOjoAbierto5-1");
-  var ojoCerradoConf5 = $("#imagenOjoCerrado5-1");
-
-  //Lo mismo para confirmar contraseña
-  ojoCerradoConf5.on("click", function () {
-    if (inputConfContr5.attr("type") === "password") {
-      inputConfContr5.attr("type", "text");
-      ojoAbiertoConf5.prop("hidden", false);
-      ojoCerradoConf5.prop("hidden", true);
-    }
-  });
-
-  ojoAbiertoConf5.on("click", function () {
-    if (inputConfContr5.attr("type") === "text") {
-      inputConfContr5.attr("type", "password");
-      ojoAbiertoConf5.prop("hidden", true);
-      ojoCerradoConf5.prop("hidden", false);
-    }
-  });
-
-  //funcionalidad del ojo para el contenedor 4
-  var ojoAbierto4 = $("#imagenOjoAbierto4");
-  var ojoCerrado4 = $("#imagenOjoCerrado4");
-
-  ojoCerrado4.on("click", function () {
-    if (inputContr4.attr("type") === "password") {
-      inputContr4.attr("type", "text");
-      ojoAbierto4.prop("hidden", false);
-      ojoCerrado4.prop("hidden", true);
-    }
-  });
-
-  ojoAbierto4.on("click", function () {
-    if (inputContr4.attr("type") === "text") {
-      inputContr4.attr("type", "password");
-      ojoAbierto4.prop("hidden", true);
-      ojoCerrado4.prop("hidden", false);
-    }
-  });
-
-  var ojoAbiertoConf4 = $("#imagenOjoAbierto4-1");
-  var ojoCerradoConf4 = $("#imagenOjoCerrado4-1");
-  //Lo mismo para confirmar contraseña
-  ojoCerradoConf4.on("click", function () {
-    if (inputConfContr4.attr("type") === "password") {
-      inputConfContr4.attr("type", "text");
-      ojoAbiertoConf4.prop("hidden", false);
-      ojoCerradoConf4.prop("hidden", true);
-    }
-  });
-
-  ojoAbiertoConf4.on("click", function () {
-    if (inputConfContr4.attr("type") === "text") {
-      inputConfContr4.attr("type", "password");
-      ojoAbiertoConf4.prop("hidden", true);
-      ojoCerradoConf4.prop("hidden", false);
-    }
-  });
 
   //Hasta que no estén todos los campos rellenos, el botón estará deshabilitado
-  $(
-    "#inputNombre5, #inputCorreo5, #inputContrasena5, #inputConfContrasena5, #check5"
-  ).on("input", function () {
-    var nombreValor = inputNombre5.val();
-    var emailValor = inputMail5.val();
-    var contValor = inputContr5.val();
-    var confContValor = inputConfContr5.val();
-    var check5 = $("#check5").prop("checked");
-    if (
-      nombreValor !== "" &&
-      emailValor !== "" &&
-      contValor !== "" &&
-      confContValor !== "" &&
-      check5 === true
-    ) {
-      botonCrear5.prop("disabled", false);
-    } else {
-      botonCrear5.prop("disabled", true);
+  $("#nombre5, #correo5, #contrasena5, #confContrasena5").on(
+    "input",
+    function () {
+      var nombreValor = inputNombre5.val();
+      var emailValor = inputMail5.val();
+      var contValor = inputContr5.val();
+      var confContValor = inputConfContr5.val();
+      if (
+        nombreValor !== "" &&
+        emailValor !== "" &&
+        contValor !== "" &&
+        confContValor !== ""
+      ) {
+        botonCrear5.prop("disabled", false);
+      } else {
+        botonCrear5.prop("disabled", true);
+      }
     }
-  });
-  //Error blur nombre
-  inputNombre5.blur(function () {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error");
-      inputNombre5.addClass("inputError");
-      inputNombre5.attr("placeholder", "Máximo de 10 caracteres");
-      $("#nombreError5").prop("hidden", false);
-    } else {
-      $(this).removeClass("error");
-      inputNombre5.removeClass("inputError");
-      $("#nombreError5").prop("hidden", true);
-    }
-  });
-
-  //Error blur email
-  inputMail5.blur(function () {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error");
-      inputMail5.addClass("inputError");
-      $("#mailError5").prop("hidden", false);
-    } else {
-      $(this).removeClass("error");
-      inputMail5.removeClass("inputError");
-      $("#mailError5").prop("hidden", true);
-    }
-  });
-  var divContrasena = $("#ojo5");
-
-  //Error blur contraseña
-  inputContr5.blur(function () {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error");
-      divContrasena.addClass("inputError");
-      inputContr5.addClass("inputError");
-      $("#contError5").prop("hidden", false);
-    } else {
-      $(this).removeClass("error");
-      inputContr5.removeClass("inputError");
-      $("#contError5").prop("hidden", true);
-      divContrasena.removeClass("inputError");
-    }
-  });
-  var divConfContrasena = $("#ojoConf5");
-  //Error blur confirmar contraseña
-  inputConfContr5.blur(function () {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error");
-      divConfContrasena.addClass("inputError");
-      inputConfContr5.addClass("inputError");
-      $("#confContError5").prop("hidden", false);
-    } else {
-      $(this).removeClass("error");
-      inputConfContr5.removeClass("inputError");
-      $("#confContError5").prop("hidden", true);
-      divConfContrasena.removeClass("inputError");
-    }
-  });
+  );
 
   var inputNombre4 = $("#inputNombre4");
-  var inputMail4 = $("#inputCorreo4");
-  var inputContr4 = $("#inputContrasena4");
-  var inputConfContr4 = $("#inputConfContrasena4");
-  var inputFecha4 = $("#InputNacimiento4");
+  var inputMail4 = $("#correo4");
+  var inputContr4 = $("#contrasena4");
+  var inputConfContr4 = $("#confContrasena4");
+  var inputFecha4 = $("#nacimiento4");
   var botonCrear4 = $("#crear4");
 
-  var formulario4 = $("#formulario4");
-
-  //Al pulsar el botón Crear, si todo ha ido bien mostrará un mensaje de éxito                                                  ---
-  botonCrear4.on("click", function (e) {
-    e.preventDefault();
-    //Si la contraseña no coincide con la confirmación, se mostrará un mensaje de error
-    if (inputContr4.val() !== inputConfContr4.val()) {
-      $("#coincidirContrasena").prop("hidden", false);
-    } else {
-      formulario4.slideUp(500, function () {
-        contenedor7.slideDown(500);
-      });
-    }
-  });
-
-  //Error blur nombre
   inputNombre4.blur(function () {
     if ($(this).val().trim() === "") {
       $(this).addClass("error");
       inputNombre4.addClass("inputError");
       inputNombre4.attr("placeholder", "Máximo de 10 caracteres");
-      $("#nombreError").prop("hidden", false);
+      $("#mensajeError").prop("hidden", false);
     } else {
       $(this).removeClass("error");
       inputNombre4.removeClass("inputError");
-      $("#nombreError").prop("hidden", true);
-    }
-  });
-
-  //Error blur email
-  inputMail4.blur(function () {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error");
-      inputMail4.addClass("inputError");
-      $("#mailError").prop("hidden", false);
-    } else {
-      $(this).removeClass("error");
-      inputMail4.removeClass("inputError");
-      $("#mailError").prop("hidden", true);
-    }
-  });
-  var divContrasena4 = $("#ojo4");
-  //Error blur contraseña
-  inputContr4.blur(function () {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error");
-      divContrasena4.addClass("inputError");
-      inputContr4.addClass("inputError");
-      $("#contError").prop("hidden", false);
-    } else {
-      $(this).removeClass("error");
-      inputContr4.removeClass("inputError");
-      $("#contError").prop("hidden", true);
-      divContrasena4.removeClass("inputError");
-    }
-  });
-  var divConfContrasena4 = $("#ojoConf4");
-  //Error blur confirmar contraseña
-  inputConfContr4.blur(function () {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error");
-      divConfContrasena4.addClass("inputError");
-      inputConfContr4.addClass("inputError");
-      $("#confContError").prop("hidden", false);
-    } else {
-      $(this).removeClass("error");
-      inputConfContr4.removeClass("inputError");
-      $("#confContError").prop("hidden", true);
-      divConfContrasena4.removeClass("inputError");
-    }
-  });
-  //Error blur fecha
-  inputFecha4.blur(function () {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error");
-      inputFecha4.addClass("inputError");
-      $("#fechaError").prop("hidden", false);
-    } else {
-      $(this).removeClass("error");
-      inputFecha4.removeClass("inputError");
-      $("#fechaError").prop("hidden", true);
+      $("#mensajeError").prop("hidden", true);
     }
   });
 
   //Hasta que no estén todos los campos rellenos, el botón estará deshabilitado
-  $(
-    "#inputNombre4, #inputCorreo4, #inputContrasena4, #inputConfContrasena4, #InputNacimiento4, #check4"
-  ).on("input", function () {
-    var nombreValor = inputNombre4.val();
-    var emailValor = inputMail4.val();
-    var contValor = inputContr4.val();
-    var confContValor = inputConfContr4.val();
-    var fechaValor = inputFecha4.val();
-    var check4 = $("#check4").prop("checked");
-    if (
-      nombreValor !== "" &&
-      emailValor !== "" &&
-      contValor !== "" &&
-      confContValor !== "" &&
-      fechaValor !== "" &&
-      check4 === true
-    ) {
-      botonCrear4.prop("disabled", false);
-    } else {
-      botonCrear4.prop("disabled", true);
+  $("#inputNombre4, #correo4, #contrasena4, #confContrasena4, #nacimiento4").on(
+    "input",
+    function () {
+      var nombreValor = inputNombre4.val();
+      var emailValor = inputMail4.val();
+      var contValor = inputContr4.val();
+      var confContValor = inputConfContr4.val();
+      var fechaValor = inputFecha4.val();
+      if (
+        nombreValor !== "" &&
+        emailValor !== "" &&
+        contValor !== "" &&
+        confContValor !== "" &&
+        fechaValor !== ""
+      ) {
+        botonCrear4.prop("disabled", false);
+      } else {
+        botonCrear4.prop("disabled", true);
+      }
     }
-  });
+  );
 });
--- Tema4/JQuery/snake/js/accion.js
+++ Tema4/JQuery/snake/js/accion.js
@@ -1,100 +1,137 @@
-$(document).ready(function(){
-  const cellSize = 10;
-  const cols = 40; // 400px / 10px
-  const rows = 60; // 600px / 10px
-  let snake = [];
-  let direction = {x: 1, y: 0}; 
-  let apple = {};
-  let gameInterval;
-  let gameSpeed = 150;
-  let running = false;
+$(document).ready(function () {
+  const tamañoCelda = 10;
+  const columnas = 40; // 400px / 10px
+  const filas = 60; // 600px / 10px
+  let serpiente = [];
+  let direccion = { x: 1, y: 0 };
+  let manzana = {};
+  let intervaloJuego;
+  let velocidadJuego = 150;
+  let corriendo = false;
 
-  function initGame(){
-    snake = [{x: Math.floor(cols/2), y: Math.floor(rows/2)}]; // Posición inicial en el centro
-    direction = {x: 1, y: 0};
-    generateApple();
+  function iniciarJuego() {
+    serpiente = [{ x: Math.floor(columnas / 2), y: Math.floor(filas / 2) }]; // Posición inicial en el centro
+    direccion = { x: 1, y: 0 };
+    generarManzana();
     $("#gameOver").hide();
-    $("#restartBtn").hide();
-    clearInterval(gameInterval);
-    running = true;
-    gameInterval = setInterval(gameLoop, gameSpeed);
-    render();
+    $("#reiniciar").hide();
+    clearInterval(intervaloJuego);
+    corriendo = true;
+    intervaloJuego = setInterval(cicloJuego, velocidadJuego);
+    renderizar();
   }
 
-  function gameLoop(){
-    let newHead = {
-      x: snake[snake.length - 1].x + direction.x,
-      y: snake[snake.length - 1].y + direction.y
+  function cicloJuego() {
+    let nuevaCabeza = {
+      x: serpiente[serpiente.length - 1].x + direccion.x,
+      y: serpiente[serpiente.length - 1].y + direccion.y,
     };
 
     // Verificar colisión con los bordes
-    if(newHead.x < 0 || newHead.x >= cols || newHead.y < 0 || newHead.y >= rows){
-      return gameOver();
+    if (
+      nuevaCabeza.x < 0 ||
+      nuevaCabeza.x >= columnas ||
+      nuevaCabeza.y < 0 ||
+      nuevaCabeza.y >= filas
+    ) {
+      return finJuego();
     }
 
     // Verificar si se muerde a sí misma
-    if(snake.some(segment => segment.x === newHead.x && segment.y === newHead.y)){
-      return gameOver();
+    if (
+      serpiente.some(
+        (segmento) =>
+          segmento.x === nuevaCabeza.x && segmento.y === nuevaCabeza.y
+      )
+    ) {
+      return finJuego();
     }
 
-    snake.push(newHead);
+    serpiente.push(nuevaCabeza);
 
     // Si la serpiente come la manzana, generar una nueva
-    if(newHead.x === apple.x && newHead.y === apple.y){
-      generateApple();
+    if (nuevaCabeza.x === manzana.x && nuevaCabeza.y === manzana.y) {
+      generarManzana();
     } else {
-      snake.shift(); // Mueve la serpiente eliminando la cola
+      serpiente.shift(); // Mueve la serpiente eliminando la cola
     }
 
-    render();
+    renderizar();
   }
 
-  function generateApple(){
-    let valid = false;
-    while(!valid){
-      let newApple = {
-        x: Math.floor(Math.random() * cols),
-        y: Math.floor(Math.random() * rows)
+  function generarManzana() {
+    let valido = false;
+    while (!valido) {
+      let nuevaManzana = {
+        x: Math.floor(Math.random() * columnas),
+        y: Math.floor(Math.random() * filas),
       };
-      if(!snake.some(segment => segment.x === newApple.x && segment.y === newApple.y)){
-        apple = newApple;
-        valid = true;
+      if (
+        !serpiente.some(
+          (segmento) =>
+            segmento.x === nuevaManzana.x && segmento.y === nuevaManzana.y
+        )
+      ) {
+        manzana = nuevaManzana;
+        valido = true;
       }
     }
   }
 
-  function render(){
+  function renderizar() {
     $("#gameBoard").empty();
 
-    snake.forEach(segment => {
-      const $segment = $("<div class='snake'></div>");
-      $segment.css({ left: segment.x * cellSize, top: segment.y * cellSize });
-      $("#gameBoard").append($segment);
+    serpiente.forEach((segmento) => {
+      const $segmento = $("<div class='serpiente'></div>");
+      $segmento.css({
+        left: segmento.x * tamañoCelda,
+        top: segmento.y * tamañoCelda,
+      });
+      $("#gameBoard").append($segmento);
     });
 
-    const $apple = $("<div class='apple'></div>");
-    $apple.css({ left: apple.x * cellSize, top: apple.y * cellSize });
-    $("#gameBoard").append($apple);
+    const $manzana = $("<div class='manzana'></div>");
+    $manzana.css({
+      left: manzana.x * tamañoCelda,
+      top: manzana.y * tamañoCelda,
+    });
+    $("#gameBoard").append($manzana);
   }
 
-  function gameOver(){
-    running = false;
-    clearInterval(gameInterval);
+  function finJuego() {
+    corriendo = false;
+    clearInterval(intervaloJuego);
     $("#gameOver").show();
-    $("#restartBtn").show();
+    $("#reiniciar").show();
   }
 
-  $(document).keydown(function(e){
-    if(!running) return;
-    switch(e.which) {
-      case 37: if(direction.x !== 1) { direction = {x: -1, y: 0}; } break; // Izquierda
-      case 38: if(direction.y !== 1) { direction = {x: 0, y: -1}; } break; // Arriba
-      case 39: if(direction.x !== -1) { direction = {x: 1, y: 0}; } break; // Derecha
-      case 40: if(direction.y !== -1) { direction = {x: 0, y: 1}; } break; // Abajo
+  $(document).keydown(function (e) {
+    if (!corriendo) return;
+    switch (e.which) {
+      case 37:
+        if (direccion.x !== 1) {
+          direccion = { x: -1, y: 0 };
+        }
+        break; // Izquierda
+      case 38:
+        if (direccion.y !== 1) {
+          direccion = { x: 0, y: -1 };
+        }
+        break; // Arriba
+      case 39:
+        if (direccion.x !== -1) {
+          direccion = { x: 1, y: 0 };
+        }
+        break; // Derecha
+      case 40:
+        if (direccion.y !== -1) {
+          direccion = { x: 0, y: 1 };
+        }
+        break; // Abajo
     }
     e.preventDefault();
   });
 
-  $("#startBtn").click(initGame);
-  $("#restartBtn").click(initGame);
+  $("#iniciar").click(iniciarJuego);
+  $("#reiniciar").click(iniciarJuego);
 });
