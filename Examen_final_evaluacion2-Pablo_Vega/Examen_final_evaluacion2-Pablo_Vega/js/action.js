9a8fb63182635890c5d4e1381a7017b01252129c
pvega7721 <pvega7721@ceuandalucia.es>
2025-02-20 18:18:00 +0100

    .

---
+++ Examen_final_evaluacion2-Pablo_Vega/Examen_final_evaluacion2-Pablo_Vega/js/action.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  var aceptarCookies = $("#aceptar");
+  var contenedorCookies = $("#cookies");
+
+  aceptarCookies.on("click", function () {
+    contenedorCookies.fadeOut();
+  });
+});
